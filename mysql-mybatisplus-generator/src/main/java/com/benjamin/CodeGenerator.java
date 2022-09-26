504cdb0400f074e897d1be96ec28701d802aa091
zhoubenjian <bjzhou@outlook.com>
2022-09-26 22:45:35 +0800

    add mysql-mybatis-generator

---
+++ mysql-mybatisplus-generator/src/main/java/com/benjamin/CodeGenerator.java
@@ -0,0 +1,142 @@
+package com.benjamin;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CodeGenerator {
+
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotBlank(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        // 获取工程的根目录
+        String projectPath = System.getProperty("user.dir");    // 得到当前项目的路径
+        gc.setOpen(false);                                      // 是否生成代码后打开本地目录
+        gc.setSwagger2(true);                                   // 实体属性 Swagger2 注解
+        gc.setOutputDir(projectPath + "/centos-mysql-mybatisplus-shiro/src/main/java");//设置代码生成路径
+        gc.setFileOverride(true);                               // 是否覆盖以前文件
+        gc.setOpen(false);                                      // 是否打开生成目录
+        gc.setAuthor("benjamin");                               // 设置项目作者名称
+        gc.setIdType(IdType.AUTO);                              // 设置主键策略
+        gc.setBaseResultMap(true);                              // 生成基本ResultMap
+        gc.setBaseColumnList(true);                             // 生成基本ColumnList
+        gc.setServiceName("%sService");                         // 去掉服务默认前缀
+        gc.setDateType(DateType.ONLY_DATE);                     // 设置时间类型
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/usa?serverTimezone=GMT%2B8&characterEncoding=utf8&useUnicode=true&useSSL=false");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");          // 这是mysql8.0以上版本的（ysql是5.7版本：com.mysql.jdbc.Driver）
+        dsc.setUsername("root");
+        dsc.setPassword("xia0yu");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+//        pc.setModuleName("mybatis-mysql-mybatisplus-api");
+        pc.setParent("com.mybatisplus");                        // 设置父包
+        pc.setMapper("dao");
+//        pc.setXml("mapper.xml");
+        pc.setEntity("entities");
+        pc.setService("service");
+        pc.setServiceImpl("service.impl");
+        pc.setController("controller");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        // String templatePath = "/templates/mapper.xml.ftl";
+        // 如果模板引擎是 velocity
+        String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/centos-mysql-mybatisplus-shiro/src/main/resources/mapper/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig sc = new StrategyConfig();
+        sc.setNaming(NamingStrategy.underline_to_camel);
+        sc.setColumnNaming(NamingStrategy.underline_to_camel);
+        sc.setEntityLombokModel(true);                          // 自动lombok
+        sc.setRestControllerStyle(true);
+        sc.setControllerMappingHyphenStyle(true);
+        sc.setLogicDeleteFieldName("deleted");                  // 设置逻辑删除
+
+        // 设置自动填充配置
+        TableFill gmt_create = new TableFill("create_time", FieldFill.INSERT);
+        TableFill gmt_modified = new TableFill("update_time", FieldFill.INSERT_UPDATE);
+        ArrayList<TableFill> tableFills = new ArrayList<>();
+        tableFills.add(gmt_create);
+        tableFills.add(gmt_modified);
+        sc.setTableFillList(tableFills);
+
+        // 乐观锁
+        sc.setVersionFieldName("version");
+        sc.setRestControllerStyle(true);        // 驼峰命名
+
+        // 手动输入
+        sc.setInclude(scanner("表名,多个英文逗号分割").split(","));
+
+        // 自动生成全部
+//        mpg.setStrategy(sc);
+
+        // 生成代码
+        mpg.execute();
+    }
+}

c923ec02c021cbe9a09b556565f4e15807d1e2a1
zhoubenjian <bjzhou@outlook.com>
2022-10-12 22:51:39 +0800

    删除冗余

--- mysql-mybatisplus-common/src/main/java/com/benjamin/swagger/Swagger2Config.java
+++ mysql-mybatisplus-common/src/main/java/com/benjamin/swagger/Swagger2Config.java
@@ -2,13 +2,11 @@ package com.benjamin.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -27,34 +25,13 @@ public class Swagger2Config {
                 .build();
     }
 
-
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
-
-
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("benjamin's Swagger2 Api ")
+                .title("Benjamin's Swagger2 Api documentation")
                 .description("Just have a little faith.")
-                .termsOfServiceUrl("http://127.0.0.1:9000/swagger.ui")
-                //.contact("")
+                .termsOfServiceUrl("www.spring.io")
+                .contact(new Contact("Benjamin", "", "bjzhou@outlook.com"))
                 .version("1.0")
                 .build();
     }
-
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addRedirectViewController("/null/api-docs",
-//                "/api-docs").setKeepQueryParams(true);
-//        registry.addRedirectViewController("/null/swagger-resources/configuration/ui",
-//                "/swagger-resources/configuration/ui");
-//        registry.addRedirectViewController("/null/swagger-resources/configuration/security",
-//                "/swagger-resources/configuration/security");
-//        registry.addRedirectViewController("/null/swagger-resources", "/swagger-resources");
-//    }
 }

0c4563c78b0f588e498e68d0c639c45104d7aa9d
zhoubenjian <bjzhou@outlook.com>
2022-10-22 13:06:51 +0800

    add Junit test

---
+++ mysql-mybatisplus-usa/src/test/java/com/benjamin/UsaTest.java
@@ -0,0 +1,32 @@
+package com.benjamin;
+
+import com.benjamin.dao.PartyMapper;
+import com.benjamin.dao.StateMapper;
+import com.benjamin.entities.Party;
+import com.benjamin.entities.State;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UsaTest {
+
+    @Autowired
+    private PartyMapper partyMapper;
+
+
+
+    @Test
+    public void stateTest() {
+        log.info("开始测试...");
+        List<Party> partyList = partyMapper.selectList(null);
+        log.info("结束测试...");
+    }
+}

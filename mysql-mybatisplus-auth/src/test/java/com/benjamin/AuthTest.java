23fd2d4c7a3baacb3b8a4c28e81fb3f4d9e80411
zhoubenjian <bjzhou@outlook.com>
2022-11-14 23:22:18 +0800

    delete some pox in mysql-mybatisplus-auth; add junit: Shiro SHA-256 salt Encode password...

--- mysql-mybatisplus-auth/src/test/java/com/benjamin/AuthTest.java
+++ mysql-mybatisplus-auth/src/test/java/com/benjamin/AuthTest.java
@@ -3,6 +3,8 @@ package com.benjamin;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.benjamin.dao.SysUserMapper;
 import com.benjamin.entities.SysUser;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +26,24 @@ public class AuthTest {
         queryWrapper.eq("user_name", "superadmin");
         SysUser sysUser = sysUserMapper.selectOne(queryWrapper);
         System.out.println(sysUser);
+    }
+
+    /**
+     * 盐值加密
+     */
+    @Test
+    public void sha() {
+        String password1 = "123456";
+        String salt = "notR1lqlQ1uF1FbyyF95N/+Gjg==";
+                // new SecureRandomNumberGenerator().nextBytes(19).toString();
+        int times = 7;
+        SimpleHash encodePassword = new SimpleHash("SHA-256", password1, salt, times);
+        System.out.println("原始密码：" + password1 + "；\n" +
+                "加密方式：SHA-256；\n" +
+                "盐值：" + salt + "；\n" +
+                "123456加密后密码：" + encodePassword);
+
+        String password2 = "12345";
+        System.out.println(" 12345加密后密码：" + new SimpleHash("SHA-256", password2, salt, times));
     }
 }

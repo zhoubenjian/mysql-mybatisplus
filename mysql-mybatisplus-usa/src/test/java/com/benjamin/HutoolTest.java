b716f88d04a8d3f65680df9469d5210ab6943dea
Benjamin <bjzhou@outlook.com>
2023-12-02 08:58:24 +0800

    add mobileTest by Hutool

--- mysql-mybatisplus-usa/src/test/java/com/benjamin/HutoolTest.java
+++ mysql-mybatisplus-usa/src/test/java/com/benjamin/HutoolTest.java
@@ -1,5 +1,6 @@
 package com.benjamin;
 
+import cn.hutool.core.lang.Validator;
 import cn.hutool.core.util.DesensitizedUtil;
 import cn.hutool.core.util.IdcardUtil;
 import org.junit.Test;
@@ -11,24 +12,41 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class HutoolTest {
 
-    private final static String idNumber = "321083197812162119";
+    private final static String ID_NUMBER = "321083197812162119";
+
+    private final static String MOBILE_PHONE = "19999999999";
 
 
 
+    /**
+     * 身份证号操作
+     */
     @Test
     public void idCardTest() {
 
         // 是否合法
-        System.out.println(IdcardUtil.isValidCard(idNumber));
+        System.out.println(IdcardUtil.isValidCard(ID_NUMBER));
 
         // 省份
-        System.out.println(IdcardUtil.getProvinceByIdCard(idNumber));
+        System.out.println(IdcardUtil.getProvinceByIdCard(ID_NUMBER));
 
         // 年龄
-        System.out.println(IdcardUtil.getAgeByIdCard(idNumber));
+        System.out.println(IdcardUtil.getAgeByIdCard(ID_NUMBER));
 
         // 脱敏（front：前面隐藏的位数；end：后面隐藏的位数）
-        System.out.println(DesensitizedUtil.idCardNum(idNumber, 6, 4));
+        System.out.println(DesensitizedUtil.idCardNum(ID_NUMBER, 6, 4));
+    }
 
+    /**
+     * 手机号操作
+     */
+    @Test
+    public void mobileTest() {
+
+        // 是否合法
+        System.out.println(Validator.isMobile(MOBILE_PHONE));
+
+        // 脱敏
+        System.out.println(DesensitizedUtil.mobilePhone(MOBILE_PHONE));
     }
 }

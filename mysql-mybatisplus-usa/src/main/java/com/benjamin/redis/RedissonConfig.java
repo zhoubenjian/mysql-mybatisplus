bc4d9a19a80dad2dd50fd9cbba116d4637f505cf
zhoubenjian <bjzhou@outlook.com>
2025-04-11 20:55:45 +0800

    update and fix something...

--- mysql-mybatisplus-usa/src/main/java/com/benjamin/redis/RedissonConfig.java
+++ mysql-mybatisplus-usa/src/main/java/com/benjamin/redis/RedissonConfig.java
@@ -1,25 +1,34 @@
 package com.benjamin.redis;
 
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "spring.redis")
+//@ConfigurationProperties(prefix = "spring.redis")
 public class RedissonConfig {
 
+    @Value("${spring.redis.database}")
     private Integer database;
 
+    @Value("${spring.redis.host}")
     private String host;
 
+    @Value("${spring.redis.port}")
     private Integer port;
 
+    @Value("${spring.redis.password}")
     private String password;
+
+    @Value("${spring.redis.timeout}")
+    private Integer timeout;
 
 
 
@@ -28,7 +37,13 @@ public class RedissonConfig {
         Config config = new Config();
         config.useSingleServer()
                 .setDatabase(database)
-                .setAddress("redis://" + host + ":" + port);
+                .setAddress("redis://" + host + ":" + port)
+                .setTimeout(timeout);
+
+        // 密码不为空时，设置密码
+        if (StringUtils.isNotEmpty(password))
+            config.useSingleServer().setPassword(password);
+
         RedissonClient redisson = Redisson.create(config);
         return redisson;
     }
